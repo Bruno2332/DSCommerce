1a44079ee9794a194d04a6a7f78417b0294335e2
Bruno Flor de Lys <brunolys23@gmail.com>
2024-12-21 16:46:09 -0300

    GET product by id

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-NSERT INTO tb_category(name) VALUES ('Livros');
+INSERT INTO tb_category(name) VALUES ('Livros');
 INSERT INTO tb_category(name) VALUES ('Eletrônicos');
 INSERT INTO tb_category(name) VALUES ('Computadores');
 
