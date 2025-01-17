cec3c305249aef05c8456528f750b6f23683e706
Bruno2332 <brunolys23@gmail.com>
2025-01-17 20:11:01 -0300

    Get user logged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -31,7 +31,6 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 
-	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
